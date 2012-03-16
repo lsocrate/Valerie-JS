83a31605c0751d0304449d21902f906f433a1a33
Luiz Socrate <luiz@socrate.com.br>
2012-03-16 15:46:49 -0300

    Basic character model

---
+++ models/character.js
@@ -0,0 +1,42 @@
+Character = function(){
+  this.name         = '';
+  this.clan         = '';
+  this.bloodline    = '';
+  this.covenants    = [];
+  this.attributes   = {};
+  this.skills       = {};
+  this.merits       = [];
+  this.disciplines  = [];
+  this.humanity     = 7;
+  this.derangements = [];
+  this.willpower    = {};
+  this.vitae        = 8;
+  this.bloodPotence = 1;
+  this.health       = {};
+  this.assets       = [];
+};
+
+Character.prototype.joinCovenant = function(covenant){
+  if(typeof covenant == 'string'){
+    this.covenants.push(covenant);
+  }
+
+  return this;
+};
+
+Character.prototype.gainMerit = function(merit){
+  var level,
+      description;
+
+  level = (typeof arguments[1] === 'number') ? arguments[1] :
+          (typeof arguments[2] === 'number') ? arguments[2] :
+          1;
+
+  description = (typeof arguments[1] === 'string') ? arguments[1] :
+                (typeof arguments[2] === 'string') ? arguments[2] :
+                null;
+
+  this.merits.push({name:merit,level:level,description:description});
+};
+
+exports.Character = Character;
